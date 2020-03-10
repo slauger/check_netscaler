a7171321a52a95be0be91ec23add3eae3bf953c1
Simon Lauger <simon@lauger.de>
2020-03-10 16:55:05 +0100

    fix path to tests/license

--- tests/travis_prepare.sh
+++ tests/travis_prepare.sh
@@ -10,7 +10,7 @@ function deploy_nsconfig
   # configure netscaler cpx
   sshpass -p nsroot scp tests/ns.conf nsroot@${NETSCALER_IP}:/home/nsroot/ns.conf
   sshpass -p nsroot ssh nsroot@${NETSCALER_IP} "/var/netscaler/bins/cli_script.sh /home/nsroot/ns.conf"
-  sshpass -p nsroot scp tests/license/license/*.lic /nsconfig/license/
+  sshpass -p nsroot scp tests/license/*.lic /nsconfig/license/
 }
 
 deploy_nsconfig
