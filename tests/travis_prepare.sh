885e13b058c8ff82d91de7303d5a63ac358395f7
Simon Lauger <simon@lauger.de>
2020-07-23 00:05:49 +0200

    back to port 22

--- tests/travis_prepare.sh
+++ tests/travis_prepare.sh
@@ -7,13 +7,13 @@ set -xeo pipefail
 function deploy_nsconfig
 {
   # auto accept ssh host key
-  sshpass -p nsroot ssh -p 2222 -o SendEnv=None -o StrictHostKeyChecking=no nsroot@${NETSCALER_IP} echo
+  sshpass -p nsroot ssh -p 22 -o SendEnv=None -o StrictHostKeyChecking=no nsroot@${NETSCALER_IP} echo
 
   # configure netscaler cpx
-  sshpass -p nsroot ssh -p 2222 -o SendEnv=None nsroot@${NETSCALER_IP} "mkdir -p /nsconfig/license"
-  sshpass -p nsroot scp -p 2222 -o SendEnv=None tests/license/*.lic nsroot@${NETSCALER_IP}:/nsconfig/license/
-  sshpass -p nsroot scp -p 2222 -o SendEnv=None tests/ns.conf nsroot@${NETSCALER_IP}:/home/nsroot/ns.conf
-  sshpass -p nsroot ssh -p 2222 -o SendEnv=None nsroot@${NETSCALER_IP} "/var/netscaler/bins/cli_script.sh /home/nsroot/ns.conf"
+  sshpass -p nsroot ssh -p 22 -o SendEnv=None nsroot@${NETSCALER_IP} "mkdir -p /nsconfig/license"
+  sshpass -p nsroot scp -p 22 -o SendEnv=None tests/license/*.lic nsroot@${NETSCALER_IP}:/nsconfig/license/
+  sshpass -p nsroot scp -p 22 -o SendEnv=None tests/ns.conf nsroot@${NETSCALER_IP}:/home/nsroot/ns.conf
+  sshpass -p nsroot ssh -p 22 -o SendEnv=None nsroot@${NETSCALER_IP} "/var/netscaler/bins/cli_script.sh /home/nsroot/ns.conf"
 }
 
 if [ -z "${NETSCALER_IP}" ]; then
